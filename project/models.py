1b032323c65a5362356ba35c9d14a8c210aad51b
Maurício Servatius Hendrikx <mauricio.servatius@gmail.com>
2025-07-09 13:40:16 -0300

    main

--- project/models.py
+++ project/models.py
@@ -3,6 +3,10 @@ from . import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    admin = db.Column(db.Integer)
+    name = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(1000))
-    name = db.Column(db.String(100))
+    mobile = db.Column(db.String(30))
+    language = db.Column(db.String(2))
+    theme = db.Column(db.String(5))
